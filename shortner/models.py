38d19633492843e56320aef0b63baae5b18f215e
Ajatta-Samuel <110544042+ajatta-samuel@users.noreply.github.com>
2022-09-22 03:17:45 +1200

    the first time

---
+++ shortner/models.py
@@ -0,0 +1,8 @@
+import uuid
+from django.db import models
+
+# Create your models here.
+class Url(models.Model):
+    link = models.CharField(max_length=1000)
+    uuid = models.CharField(max_length=10)
+
